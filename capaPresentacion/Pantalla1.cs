730d61b4a51d0879de347369fe32986ba0a4cbd9
Samuel Jimenez <samuelelias13@hotmail.com>
2023-03-14 22:05:04 -0400

    Primer commit

--- capaPresentacion/Pantalla1.cs
+++ capaPresentacion/Pantalla1.cs
@@ -40,12 +40,12 @@ namespace capaPresentacion
 
         private void textBox2_TextChanged(object sender, EventArgs e)
         {
-
+            
         }
 
         private void label3_Click(object sender, EventArgs e)
         {
-
+            
         }
 
         private void label1_Click(object sender, EventArgs e)
